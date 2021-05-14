9a73c6d6bd03f4239ff51f97728eac468d026808
devAltss <altoss11@gmail.com>
2021-05-15 01:43:18 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,5 @@
 $(function(){
 
-
   $(".rate-star").rateYo({
     rating: 5,
     starWidth: "12px",
@@ -11,7 +10,31 @@ $(function(){
     dots: true,
     arrows: false,
     slidesToShow: 4,
-    slidesToScroll: 4
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1 
+        }
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1 
+        }
+      },
+    ]
   });
 
   $('.product-one__tabs .tab, settings__tabs .tab').on('click', function(event) {
